175c0ac476cc512f2d735a56a520a378f3e2b4e3
khanzahPuthenPeediakkal <khanzahkz004@gmail.com>
2025-07-24 18:26:52 +0100

    Update WebDriverFactory.cs

--- REMWasteTestAutomationProject/UiTestAutomation/DriverHelper/WebDriverFactory.cs
+++ REMWasteTestAutomationProject/UiTestAutomation/DriverHelper/WebDriverFactory.cs
@@ -5,12 +5,27 @@ using WebDriverManager.DriverConfigs.Impl;
 
 namespace UiTestAutomation.DriverHelper
 {
-    public class WebDriverFactory
+    public class WebDriverFactory{
+    public static IWebDriver Create(){
+    new DriverManager().SetUpDriver(new ChromeConfig());
+    var options = new ChromeOptions();
+    var tempUserDataDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+    Directory.CreateDirectory(tempUserDataDir); // Ensure the directory exists
+    options.AddArgument($"--user-data-dir={tempUserDataDir}");
+
+    options.AddArgument("--no-sandbox");
+    options.AddArgument("--disable-dev-shm-usage");
+    options.AddArgument("--disable-extensions");
+
+    if (Environment.GetEnvironmentVariable("CI") == "true")
     {
-        public static IWebDriver Create()
-        {
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            return new ChromeDriver();
-        }
+        options.AddArgument("--headless=new");
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--window-size=1920,1080");
+    }
+
+    return new ChromeDriver(options);
+    }
+
     }
 }
