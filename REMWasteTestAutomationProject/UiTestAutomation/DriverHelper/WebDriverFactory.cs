4a81c76ec82206dd40d9adde53b43b9f820d7c64
LAPTOP-JRPE26EB\kenza's <khanzahkz004@gmail.com>
2025-07-24 18:11:29 +0100

    Fix driver issue

--- REMWasteTestAutomationProject/UiTestAutomation/DriverHelper/WebDriverFactory.cs
+++ REMWasteTestAutomationProject/UiTestAutomation/DriverHelper/WebDriverFactory.cs
@@ -10,7 +10,19 @@ namespace UiTestAutomation.DriverHelper
         public static IWebDriver Create()
         {
             new DriverManager().SetUpDriver(new ChromeConfig());
-            return new ChromeDriver();
+
+            var options = new ChromeOptions();
+            var tempUserDataDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            options.AddArgument($"--user-data-dir={tempUserDataDir}");
+           
+            if (Environment.GetEnvironmentVariable("CI") == "true")
+            {
+                options.AddArgument("--headless=new");
+                options.AddArgument("--disable-gpu");
+                options.AddArgument("--window-size=1920,1080");
+            }
+            return new ChromeDriver(options);
         }
+
     }
 }
